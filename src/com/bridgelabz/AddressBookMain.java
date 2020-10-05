a6818a4351c5a26ab83058178f6bbdb05c741daa
arjun17697 <arjun17697@gmail.com>
2020-10-05 20:07:49 +0530

    Add:UC11 Sort by Name

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -135,20 +135,32 @@ public class AddressBookMain {
 				.filter(map -> getCityname.equals(map.getValue()));
 		System.out.println("Following persons belong to provided city:" + result);
 	}
+
+	// Count by State
 	private static void CountbyState() {
 		System.out.println("Enter the name of the state");
-		String getStatename= stdlin.next();
-		int count=Collections.frequency(deathNote.values(), getStatename);
-		System.out.println(count);
-		
-	}
-	private static void CountbyCity() {
-		System.out.println("Enter the name of the city");
-		String getCityname= stdlin.next();
-		int count=Collections.frequency(deathNote.values(), getCityname);
+		String getStatename = stdlin.next();
+		int count = Collections.frequency(deathNote.values(), getStatename);
 		System.out.println(count);
 
 	}
+
+	// Count by city
+	private static void CountbyCity() {
+		System.out.println("Enter the name of the city");
+		String getCityname = stdlin.next();
+		int count = Collections.frequency(deathNote.values(), getCityname);
+		System.out.println(count);
+
+	}
+
+	// Sort by first name
+	private static void SortByFirstName(ArrayList<Info> friends) {
+		Stream<Info> firstNameBasis =  friends.stream().sorted();
+		System.out.println(firstNameBasis);
+
+	}
+
 	public static void main(String[] args) {
 
 		Scanner stdlin = new Scanner(System.in);
@@ -159,7 +171,8 @@ public class AddressBookMain {
 		// initiating user functions of entries
 
 		String user_input = "1";
-		while ((user_input.equals("1") || user_input.equals("2") || user_input.equals("3"))) {
+		while ((user_input.equals("1") || user_input.equals("2") || user_input.equals("3") || user_input.equals("4")
+				|| user_input.equals("5") || user_input.equals("6") || user_input.equals("7"))) {
 
 			// Checking in address list is present in hashmap
 			System.out.print("Enter the Name of the Address Book: ");
@@ -182,7 +195,6 @@ public class AddressBookMain {
 			System.out.println("5.Sort by State");
 			System.out.println("6.Count by State");
 			System.out.println("7.Sort by City");
-			
 
 			user_input = stdlin.next();
 
@@ -213,13 +225,17 @@ public class AddressBookMain {
 			case "5":
 				SortCity();
 				break;
-				
+
 			case "6":
 				CountbyState();
 				break;
-				
+
 			case "7":
 				CountbyCity();
+				break;
+
+			case "8":
+				SortByFirstName(friends);
 				break;
 
 			default:
